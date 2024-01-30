62d3399dc8d6500b8aa367d394df704d37b4bfdc
Gabriel Figueiredo <gabrielfigueiredov@gmail.com>
2024-01-30 16:17:21 -0300

    Changed paymentservice to an http server

--- src/paymentservice/index.js
+++ src/paymentservice/index.js
@@ -1,59 +1,62 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
-const grpc = require('@grpc/grpc-js')
-const protoLoader = require('@grpc/proto-loader')
-const health = require('grpc-js-health-check')
+const express = require('express');
+
 const opentelemetry = require('@opentelemetry/api')
 
 const charge = require('./charge')
 const logger = require('./logger')
 
-function chargeServiceHandler(call, callback) {
+async function closeGracefully(signal) {
+  server.close()
+  process.kill(process.pid, signal)
+}
+
+const app = express();
+app.use(express.json())
+
+const router = express.Router();
+
+router.get('/health', (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: 'Ok',
+    date: new Date()
+  }
+
+  res.status(200).send(data);
+});
+
+router.post('/api/v1/pay', (req, res) => {
   const span = opentelemetry.trace.getActiveSpan();
 
   try {
-    const amount = call.request.amount
+    if (req.body == null || req.body.amount == null || req.body.creditCard == null) {
+      throw new Error('Invalid input.');
+    }
+
+    const amount = req.body.amount
     span.setAttributes({
       'app.payment.amount': parseFloat(`${amount.units}.${amount.nanos}`)
     })
-    logger.info({ request: call.request }, "Charge request received.")
+    logger.info({ request: req.body }, "Charge request received.")
 
-    const response = charge.charge(call.request)
-    callback(null, response)
-
+    const response = charge.charge(req.body)
+    res.status(200).send(response)
   } catch (err) {
     logger.warn({ err })
 
     span.recordException(err)
     span.setStatus({ code: opentelemetry.SpanStatusCode.ERROR })
 
-    callback(err)
+    res.status(400).send({ error: err.message })
   }
-}
+});
 
-async function closeGracefully(signal) {
-  server.forceShutdown()
-  process.kill(process.pid, signal)
-}
+app.use(router);
 
-const otelDemoPackage = grpc.loadPackageDefinition(protoLoader.loadSync('demo.proto'))
-const server = new grpc.Server()
-
-server.addService(health.service, new health.Implementation({
-  '': health.servingStatus.SERVING
-}))
-
-server.addService(otelDemoPackage.oteldemo.PaymentService.service, { charge: chargeServiceHandler })
-
-server.bindAsync(`0.0.0.0:${process.env['PAYMENT_SERVICE_PORT']}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
-  if (err) {
-    return logger.error({ err })
-  }
-
-  logger.info(`PaymentService gRPC server started on port ${port}`)
-  server.start()
-}
-)
+const port = Number(process.env.PAYMENT_SERVICE_PORT)
+const server = app.listen(port, () => logger.info("Starting server on port %d", port))
 
 process.once('SIGINT', closeGracefully)
 process.once('SIGTERM', closeGracefully)
