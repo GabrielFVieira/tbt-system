1ec29fb4d936181a7a885762fa1d0933e0595f5b
Gabriel Figueiredo <gabrielfigueiredov@gmail.com>
2024-01-30 18:00:05 -0300

    Added malabi instrumentation

--- src/paymentservice/charge.js
+++ src/paymentservice/charge.js
@@ -44,7 +44,7 @@ module.exports.charge = request => {
 
   // check baggage for synthetic_request=true, and add charged attribute accordingly
   const baggage = propagation.getBaggage(context.active());
-  if (baggage && baggage.getEntry("synthetic_request") && baggage.getEntry("synthetic_request").value === "true") {
+  if (baggage?.getEntry("synthetic_request")?.value === "true") {
     span.setAttribute('app.payment.charged', false);
   } else {
     span.setAttribute('app.payment.charged', true);
